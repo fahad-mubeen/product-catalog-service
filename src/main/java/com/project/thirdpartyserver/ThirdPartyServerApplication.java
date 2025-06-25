98e0e4c272344ad698e7c5207be8d63a66c3af51
Fahad Mubeen <fahad660606@gmail.com>
2025-06-25 13:04:11 +0300

    setup env file and config

---
+++ src/main/java/com/project/thirdpartyserver/ThirdPartyServerApplication.java
@@ -0,0 +1,19 @@
+package com.project.thirdpartyserver;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyServerApplication {
+
+    public static void main(String[] args) {
+
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(ThirdPartyServerApplication.class, args);
+    }
+
+}
